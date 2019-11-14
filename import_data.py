64afccddb0e8aa87f9a6554d04160c48c41fa7d7
Klay Kulik <klaykulik@gmail.com>
2019-11-14 13:08:41 -0500

    old data visualization

--- import_data.py
+++ import_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import glob
+import numpy as np
+import matplotlib.pyplot as plt
 
 def importData():
 
@@ -8,7 +10,7 @@ def importData():
     all_files = glob.glob(path + '*.txt')
 
     for i, filename in enumerate(all_files):
-        df = pd.read_csv(filename,sep = ' *\| *');
+        df = pd.read_csv(filename);
         all_data.append(df)
 
     complete = pd.concat(all_data)
@@ -16,6 +18,23 @@ def importData():
 
 if __name__ == '__main__':
 
-    everything = importData()
-    print(everything.head())
-    print(len(everything))
+    gal = importData()
+    # print(gal.head())
+    # print(len(gal))
+
+
+
+    X = gal.Velocity * np.sin(gal.RA) * np.cos(gal.DEC)
+    Y = gal.Velocity * np.sin(gal.RA) * np.sin(gal.DEC)
+    Z = gal.Velocity * np.cos(gal.DEC) 
+
+    from mpl_toolkits.mplot3d import Axes3D
+
+
+
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(X, Y, Z)
+    plt.show()
